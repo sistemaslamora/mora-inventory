febd81995bd2f205a002da779caf51114f1bf545
Jean Newman <newmanrios@gmail.com>
2023-03-12 18:36:33 -0500

    build all

---
+++ public/assets/use-quasar.d05cac39.js
@@ -0,0 +1,1 @@
+import{y as s,c as o,h as t,z as n,i as u,bm as c}from"./index.27345aab.js";var l=s({name:"QTr",props:{props:Object,noHover:Boolean},setup(r,{slots:e}){const a=o(()=>"q-tr"+(r.props===void 0||r.props.header===!0?"":" "+r.props.__trClass)+(r.noHover===!0?" q-tr--no-hover":""));return()=>t("tr",{class:a.value},n(e.default))}});function m(){return u(c)}export{l as Q,m as u};
